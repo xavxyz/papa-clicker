b8ebb8c4564a5b75abdc97764614a51217798380
neohipper <neohipper@gmail.com>
2016-02-22 22:52:43 +0100

    Put some things from templates into the layout

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -4,5 +4,20 @@ Template.layout.onCreated(function () {
 			this.subscribe('bonus.all');
 		}
 	});
+
+	//Init the reactie var for the options state
+	this.OptionsVisible = new ReactiveVar(false);
 });
 
+Template.layout.helpers({
+	optionsVisible: function(){
+		return Template.instance().OptionsVisible.get();
+	}
+});
+
+Template.layout.events({
+  "click #toggleOptions": function(e, template){
+    e.preventDefault();
+    template.OptionsVisible.set(!template.OptionsVisible.get());
+  }
+});
