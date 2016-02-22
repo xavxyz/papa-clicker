a76000544f05364530e3d9fdd032882e6c2871e7
neohipper <neohipper@gmail.com>
2016-02-22 23:29:56 +0100

    Added stats popup

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -7,11 +7,16 @@ Template.layout.onCreated(function () {
 
 	//Init the reactie var for the options state
 	this.OptionsVisible = new ReactiveVar(false);
+	//Init the reactie var for the stats state
+	this.StatsVisible = new ReactiveVar(false);
 });
 
 Template.layout.helpers({
 	optionsVisible: function(){
 		return Template.instance().OptionsVisible.get();
+	},
+	statsVisible: function(){
+		return Template.instance().StatsVisible.get();
 	},
 	bonusList () {
 		return Bonus.find({}).fetch();
@@ -22,5 +27,9 @@ Template.layout.events({
   "click #toggleOptions": function(e, template){
     e.preventDefault();
     template.OptionsVisible.set(!template.OptionsVisible.get());
+  },
+	"click #toggleStats": function(e, template){
+    e.preventDefault();
+    template.StatsVisible.set(!template.StatsVisible.get());
   }
 });
