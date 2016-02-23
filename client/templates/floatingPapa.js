12430e7781044b3c642316316b77189f3ec1a5d8
Chris De Rouck <chris@2minds.be>
2016-02-23 16:49:46 +0100

    added some mojo into the mix

--- client/templates/floatingPapa.js
+++ client/templates/floatingPapa.js
@@ -1,10 +1,12 @@
 Template.floatingPapa.events({
     'click #the_floating_papa': function (event, template) {
         event.preventDefault();
-        Modules.client.users.papaClicked(1);
+        //Modules.client.users.papaClicked(1);
     }
 });
 
 Template.floatingPapa.onRendered(function(){
     $('#the_floating_papa').draggable();
+
+    $('#mojo').droppable();
 });
