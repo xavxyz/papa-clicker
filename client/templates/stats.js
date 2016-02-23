ceff2d5d6cf7238fa483461981fda84349b0b669
Joni Saksholm <joni@saksholm.fi>
2016-02-23 12:40:06 +0000

    added more statistics and new bonus 'big papa'

--- client/templates/stats.js
+++ client/templates/stats.js
@@ -1,5 +1,31 @@
 Template.stats.helpers({
     totalClicks: function() {
       return Modules.client.users.getCurrentScore()
+    },
+    bonuses: function() {
+
+      let belongings = Belongings.find({userId: Meteor.userId()});
+      console.log("belongings", belongings);
+      let bonusArray = [];
+
+      belongings.forEach(function(item) {
+        let bonus = Bonus.findOne({_id: item.belongingId});
+        console.log("bonus", bonus);
+        if(bonus) {
+          let obj = {
+            belongingsAmount: item.amount,
+            bonusName: bonus.name,
+            bonusProduction: bonus.production,
+            bonusPrice: bonus.price
+          }
+
+          bonusArray.push(obj);
+        } else {
+          console.log("no any bonuses");
+        }
+
+      });
+      console.log("bonusarray", bonusArray);
+      return bonusArray;
     }
 });
